5f10a6f118b1230a2da8d5d8e5b9d5d95e857986
luisalfonsopreciado <luisapreciado99@gmail.com>
2020-09-11 17:44:07 -0500

    mongodb atlas

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,16 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
+  if (!process.env.MONGO_URI) {
+    throw new Error("Mongo URI must be defined");
+  }
   console.log("strategy-saver service starting up...");
   try {
-    // await mongoose.connect("", {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    //   useCreateIndex: true,
-    // });
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
     console.log("Connected to MongoDB");
   } catch (err) {
     console.log(err);
